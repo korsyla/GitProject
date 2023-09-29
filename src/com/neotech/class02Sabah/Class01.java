d1fd995f19cfa6914479fab48a35e83a9c733838
korsyla <korsyla@gmail.com>
2023-09-29 19:53:01 -0400

    My 4th Cmmmit

---
+++ src/com/neotech/class02Sabah/Class01.java
@@ -0,0 +1,10 @@
+package com.neotech.class02Sabah;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("2nd Class ");
+	}
+
+}
---
+++ src/com/neotech/class02Sabah/Class01Sabah.java
@@ -0,0 +1,11 @@
+package com.neotech.class02Sabah;
+
+public class Class01Sabah {
+
+	public static void main(String[] args) {
+
+		System.out.println("Seconds Class");
+		
+	}
+
+}
